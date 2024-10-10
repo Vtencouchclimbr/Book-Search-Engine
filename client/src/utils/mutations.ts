d6bb5d8582fa9fbd62819d4bea885f8d4c924ec3
Vtencouchclimbr <148500829+Vtencouchclimbr@users.noreply.github.com>
2024-10-10 16:34:35 -0600

    add mutations

--- client/src/utils/mutations.ts
+++ client/src/utils/mutations.ts
@@ -24,33 +24,23 @@ export const ADD_USER = gql`
 }
 `;
 
-export const ADD_THOUGHT = gql`
+export const SAVE_BOOK = gql`
   mutation AddThought($input: ThoughtInput!) {
     addThought(input: $input) {
-      _id
-      thoughtText
-      thoughtAuthor
+      bookId
+      title
+      authors
       createdAt
-      comments {
-        _id
-        commentText
-      }
     }
   }
 `;
 
-export const ADD_COMMENT = gql`
-  mutation addComment($thoughtId: ID!, $commentText: String!) {
-    addComment(thoughtId: $thoughtId, commentText: $commentText) {
-      _id
-      thoughtText
-      thoughtAuthor
-      createdAt
-      comments {
-        _id
-        commentText
-        createdAt
-      }
+export const REMOVE_BOOK = gql`
+  mutation RemoveBook($bookId: ID!) {
+    removeBook(bookId: $bookId) {
+      success
+      message
+      bookId
     }
   }
 `;
